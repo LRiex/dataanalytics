48a4086b6033f7d1e567c4b718ca233f7534f7ef
Varun Suryan <varunsuryan9@gmail.com>
2018-11-30 16:19:43 -0500

    linearSVM working

--- itemset.py
+++ itemset.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import random
-dfMain = pd.read_csv("Sample_Crimes2.csv")
+dfMain = pd.read_csv("Sample_Crimes3.csv")
 # print dfMain.head(3) #prints 3 heading rows
 
 # df = pd.read_csv("DayofWeek.csv")
@@ -15,16 +15,30 @@ dfMain = pd.read_csv("Sample_Crimes2.csv")
 
 indexing = random.sample(range(0, 20000), 1000)
 
-X = dfMain.values[indexing, 0:13]
-Y = dfMain.values[indexing, 13]
-print [sum(Y==i) for i in range(1,35)]
+X = dfMain.values[indexing, 0:568]
+Y = dfMain.values[indexing, 568]
+# print [sum(Y==i) for i in range(1,35)]
+
+
+# X = [list(element) for element in list(X)]
+# X = [list(map(float, element)) for element in X]
+# Y = list(map(float, Y))
+
+# print X
+# print Y
 
 from sklearn.naive_bayes import BernoulliNB
+from sklearn import svm
+lin_clf = svm.LinearSVC()
 
-clf = BernoulliNB()
-clf.fit(X, Y)
+# clf = BernoulliNB()
+lin_clf.fit(X, Y)
 # BernoulliNB(alpha=1.0, binarize=0.0, class_prior=None, fit_prior=True)
 # print X[6:800]
+print Y
+print lin_clf.predict(X)
 
+print lin_clf.decision_function
 
-print len([i for i, j in zip(clf.predict(X), Y) if i == j])
+print len([i for i, j in zip(lin_clf.predict(X), Y) if i == j])
+
