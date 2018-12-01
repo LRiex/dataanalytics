e3d78147534a4f8997be58c8a3a484e92265c5cc
Varun Suryan <varunsuryan9@gmail.com>
2018-12-01 16:15:57 -0500

    predicted label frequency chart

--- itemset.py
+++ itemset.py
@@ -10,35 +10,58 @@ dfMain = pd.read_csv("Sample_Crimes3.csv")
 # dfMain.loc[dfMain["LocationDescription"] == Nan, "LocationDescription"] = 1
 
 # dfMain.to_csv("Sample_Crimes.csv", index=False)
+trainiSize = 18000
 
 # print dfMain.values[206]
 
-indexing = random.sample(range(0, 20000), 20000)
+indexing = random.sample(range(0, 20000), trainiSize)
 
-X = dfMain.values[indexing, 0:568]
-Y = dfMain.values[indexing, 568]
+X_train = dfMain.values[indexing, 0:568]
+Y_train = dfMain.values[indexing, 568]
 # print [sum(Y==i) for i in range(1,35)]
+# print Y_train
 
+remaining = list(set(range(0, 20000)) - set(indexing))
+X_test = dfMain.values[remaining, 0:568]
+Y_test = dfMain.values[remaining, 568]
 
 # X = [list(element) for element in list(X)]
 # X = [list(map(float, element)) for element in X]
 # Y = list(map(float, Y))
-
-# print X
-# print Y
+# print remaining
+# print X_test
+# print Y_test
 
 from sklearn.naive_bayes import BernoulliNB
 from sklearn import svm
 lin_clf = svm.LinearSVC()
 
-# clf = BernoulliNB()
-lin_clf.fit(X, Y)
+# lin_clf = BernoulliNB()
+lin_clf.fit(X_train, Y_train)
 # BernoulliNB(alpha=1.0, binarize=0.0, class_prior=None, fit_prior=True)
 # print X[6:800]
-print Y
-print lin_clf.predict(X)
+# print Y
+# print lin_clf.predict(X_test)
 
-print lin_clf.decision_function
+# print lin_clf.decision_function
 
-print len([i for i, j in zip(lin_clf.predict(X), Y) if i == j])
+print 'test accuracy: ' + str(len([i for i, j in zip(lin_clf.predict(X_test), Y_test) if i == j])/float(20000 - trainiSize))
 
+print 'trainig accuracy: ' + str(len([i for i, j in zip(lin_clf.predict(X_train), Y_train) if i == j])/float(trainiSize))
+
+import csv
+RESULT = zip(remaining, list(lin_clf.predict(X_test)))
+# print RESULT
+RESULT = [list(elem) for elem in RESULT]
+# print RESULT
+
+
+with open("Sample_Crimes4.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(RESULT)
+
+
+
+# with open("output.csv",'wb', newline = '') as resultFile:
+#     wr = csv.writer(resultFile, dialect='excel')
+#     wr.writerow(remaining)
